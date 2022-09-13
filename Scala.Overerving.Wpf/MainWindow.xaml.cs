eec626d7bccd6e2d700ceb85824abe032cd76c3e
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2022-09-13 07:08:18 +0200

    Add project files.

---
+++ Scala.Overerving.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+
+
+namespace Scala.Overerving.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowPerson_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MakePersons_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
